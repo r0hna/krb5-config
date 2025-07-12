cab2f93c7e93240323583285400ea7c1d3b6fbf9
r0hna <32985298+r0hna@users.noreply.github.com>
2025-07-12 13:15:50 +0530

    initial commit

--- krb5_config.py
+++ krb5_config.py
@@ -25,9 +25,6 @@ def get_config(domain_fqdn: str, dc_name: str):
         dns_lookup_kdc = false
         ticket_lifetime = 24h
         forwardable = true
-        default_tkt_enctypes = aes256-cts-hmac-sha1-96 aes128-cts-hmac-sha1-96
-        default_tgs_enctypes = aes256-cts-hmac-sha1-96 aes128-cts-hmac-sha1-96
-        permitted_enctypes = aes256-cts-hmac-sha1-96 aes128-cts-hmac-sha1-96
 
 [realms]
         {domain_fqdn.upper()} = {{
